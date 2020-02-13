060816afe1458e98cb0a63a3b807f5b728e9fbd1
JavaCourses1 <kirill.podpruznikov@inbox.lv>
2020-02-13 19:05:31 +0200

    boolean

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,64 +4,64 @@ import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
-
 public class Main {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-
         Random rand = new Random();
-        System.out.println("Do you want to start the game? Please, type yes or no");
-        String answer = scan.next();
-        while (answer.equals("yes")) {
-
-
-        System.out.println("What is your name?");
-        String name = scan.next();
-        System.out.println("Hello," + name);
-
-        int myNum = rand.nextInt(100) + 1;
-        System.out.println("Cheat: " + myNum);
-
-        for (int i = 0; i < 10; i++) {
-
-            int userNum = askGuess();
-
-            if (myNum == userNum) {
-                System.out.println("You win");
-                break;
+        boolean answer;
+        String anotherString;
+        do {
+            System.out.println("What is your name?");
+            String name = scan.next();
+            System.out.println("Hello," + name);
+            int myNum = rand.nextInt(100) + 1;
+            System.out.println("Cheat: " + myNum);
+            for (int i = 0; i < 10; i++) {
+                int userNum = askGuess();
+                if (myNum == userNum) {
+                    System.out.println("You win");
+                    break;
+                }
+                if (i == 9) {
+                    System.out.println("Game over!! You loose!! My number was " + myNum);
+                    break;
+                }
+                if (myNum < userNum) {
+                    System.out.println("Go lower");
+                }
+                if (myNum > userNum) {
+                    System.out.println("Go higher");
+                }
             }
-
-            if (myNum < userNum) {
-                System.out.println("Go lower");
+        } while (askAnotherGame());
+        System.out.println("Goodbye");
+    }
+    static boolean askAnotherGame() {
+        for (; ; ) {
+            System.out.println("do you want to play again?");
+            String answer = scan.next();
+            if (answer.equalsIgnoreCase("yes")) {
+                return true;
+            } else if (answer.equalsIgnoreCase("no")) {
+                return false;
+            } else {
+                System.out.println("you have to answer yes or no");
             }
-
-            if (myNum > userNum) {
-                System.out.println("Go higher");
-            }
-        }
-
-        System.out.println("Do you want to play again? Please, type yes or no");
-        answer = scan.next();
-
         }
     }
-
     static int askGuess() {
         for (; ; ) {
             try {
-            System.out.println("Enter your number");
-
+                System.out.println("Enter your number");
                 int num = scan.nextInt();
-
                 if (num <= 1 || num >= 100) {
                     System.out.println("Please enter number from 1 to 100 ");
-
                 } else {
                     return num;
                 }
             } catch (InputMismatchException e) {
                 String str = scan.next();
-               System.out.println(str + " is not a number!!!");
+                System.out.println(str + " is not a number!!!");
             }
         }
     }
